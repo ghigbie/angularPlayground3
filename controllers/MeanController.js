a9047215efe52da08bee030616ee3e94f4c66a05
George Higbie <gehigbie@gmail.com>
2017-08-09 13:13:45 +0000

    Add http call

--- controllers/MeanController.js
+++ controllers/MeanController.js
@@ -1,9 +1,15 @@
 /* global angular */
 
 (function(){
-    var MeanController = function(){
+    var MeanController = function($http){
         var vm = this;
         vm.name = 'Sammy';
+        
+        $http.get('http://swapi-tpiros.rhcloud.com/films')
+            .then(function(response){
+                vm.films = response.data;
+            });
+            
     };
     
     MeanController.$inject = ['$scope'];
